2d61295baefa5de15be93041c7e22694335794ee
Faith Kariuki <tcwamboi@gmail.com>
2024-11-06 01:32:33 +0300

    Update App component to add underline style to text

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div className='text-red-500 flex justify-center text-3xl mt-5'>App</div>
+    <div className='text-red-500 flex justify-center text-3xl mt-5 underline'>App</div>
   )
 }
 
