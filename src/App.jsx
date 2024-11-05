41719748d9f7d19abc93bb46e8d303d01171351e
Faith Kariuki <tcwamboi@gmail.com>
2024-11-06 01:03:43 +0300

    Update App component style

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-red-300'>App</div>
+    <div className='text-red-500 flex justify-center text-3xl mt-5'>App</div>
   )
 }
 
