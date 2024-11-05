43b03608153764208273f54bec0facd60d2b3b3d
Faith Kariuki <tcwamboi@gmail.com>
2024-11-06 01:50:59 +0300

    Update App component to include heading for Continuous Integration test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 const App = () => {
   return (
-    <div className='text-red-500 flex justify-center text-3xl mt-5 underline'>App</div>
+    <div className='text-red-500 flex justify-center text-3xl mt-5 underline'>
+      <h1>Test Continuous Intergration</h1>
+    </div>
   )
 }
 
