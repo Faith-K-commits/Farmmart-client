4602df4cce7c237c368613893c30c9327e2f7a43
Faith Kariuki <tcwamboi@gmail.com>
2024-11-06 15:18:33 +0300

    Set up testing environment with Vitest and update App component tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import React from 'react';
+
 const App = () => {
   return (
-    <div className='text-red-500 flex justify-center text-3xl mt-5 underline'>
-      <h1>Test Continuous Intergration</h1>
+    <div className="text-red-500 flex justify-center text-3xl mt-5 underline">
+      <h1>Test Continuous Integration</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/___tests__/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    testEnvironment: 'jsdom',
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-    testPathIgnorePatterns: ['/node_modules/'],
-    moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-  };
