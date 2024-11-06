c660110b4ea7ac6934ed715e0e7088a2d39fe4bf
Faith Kariuki <tcwamboi@gmail.com>
2024-11-06 15:36:51 +0300

    Add Vitest environment setup to App component tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+/* eslint-env vitest */
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { test, expect } from 'vitest';
 import App from './App';
 
 test('renders the App component', () => {
